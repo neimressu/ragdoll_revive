8b3677c7367084cd67c6b58e311ff0ec2cbbebc7
neimressu <aleksandrovu963@gmail.com>
2026-06-15 16:27:17 +0300

    Remade player search

--- src/main/java/com/neimressu/ragdollRevive/ReviveHandler.java
+++ src/main/java/com/neimressu/ragdollRevive/ReviveHandler.java
@@ -2,19 +2,18 @@ package com.neimressu.ragdollRevive;
 
 import dev.leo.sableplayerragdoll.api.RagdollAPI;
 
+import dev.leo.sableplayerragdoll.block.entity.RagdollPartBlockEntity;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.*;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
 
@@ -41,60 +40,35 @@ public class ReviveHandler {
         if (!isReviveItem && !isExtendItem) {
             return;
         }
-        double reach = 4.0;
-        Vec3 eyePos = player.getEyePosition();
-        Vec3 lookVec = player.getViewVector(1.0F); // Вектор взгляда
-        Vec3 endPos = eyePos.add(lookVec.scale(reach));
+        HitResult hit = player.pick(5.0D, 0.0F, false);
+        if (hit.getType() != HitResult.Type.BLOCK) return;
 
-        HitResult hitResult = player.level().clip(new net.minecraft.world.level.ClipContext(
-                eyePos, endPos,
-                net.minecraft.world.level.ClipContext.Block.COLLIDER,
-                net.minecraft.world.level.ClipContext.Fluid.NONE,
-                player
-        ));
+        BlockHitResult blockHit = (BlockHitResult) hit;
+        BlockEntity be = player.level().getBlockEntity(blockHit.getBlockPos());
 
-        double actualReach = (hitResult.getType() == HitResult.Type.BLOCK) ? hitResult.getLocation().distanceTo(eyePos) : reach;
+        if (!(be instanceof RagdollPartBlockEntity ragdollPart)) return;
 
-        List<ServerPlayer> targets = player.level().getEntitiesOfClass(
-                ServerPlayer.class,
-                new AABB(eyePos, eyePos.add(lookVec.scale(actualReach))).inflate(0.5) // Небольшая область вокруг луча
-        );
+        var provider = be.getLevel().registryAccess();
+        log.info("Block NBT: {}", be.saveWithFullMetadata(provider).getAsString());
+        String skinName = ragdollPart.saveWithFullMetadata(provider).getString("SkinName");
+        log.debug(skinName);
 
-        ServerPlayer bestTarget = null;
-        double minDistance = Double.MAX_VALUE;
+        ServerPlayer target = player.server.getPlayerList().getPlayerByName(skinName);
 
-        for (ServerPlayer target : targets) {
-            if (target == player) continue;
-            if (!target.getPersistentData().getBoolean("isDying")) continue;
-            if (!RagdollAPI.isRagdolled(target)) continue;
-
-            Vec3 targetPos = target.getEyePosition();
-            Vec3 toTarget = targetPos.subtract(eyePos).normalize();
-
-            double dot = lookVec.dot(toTarget);
-            if (dot > 0.4) {
-                double dist = eyePos.distanceTo(targetPos);
-                if (dist < minDistance) {
-                    minDistance = dist;
-                    bestTarget = target;
-                }
-            }
-        }
-
-        if (bestTarget != null) {
+        if (target != null && target.getPersistentData().getBoolean("isDying")) {
             if (isExtendItem) {
-                ReviveAPI.extendReviveTime(bestTarget, ReviveManager.getExtensionTicks(itemId));
-                bestTarget.sendSystemMessage(Component.literal("Your revive time has been extended").withStyle(ChatFormatting.YELLOW));
-                player.sendSystemMessage(Component.literal("You extended revive time of " + bestTarget.getName().getString() +
+                ReviveAPI.extendReviveTime(target, ReviveManager.getExtensionTicks(itemId));
+                target.sendSystemMessage(Component.literal("Your revive time has been extended").withStyle(ChatFormatting.YELLOW));
+                player.sendSystemMessage(Component.literal("You extended revive time of " + target.getName().getString() +
                                 " by " + ReviveManager.getExtensionTicks(itemId) / 20 +
                                 " seconds")
                         .withStyle(ChatFormatting.YELLOW));
                 ReviveManager.removeItem(event.getItemStack(),player);
                 return;
             }
-            ReviveAPI.revivePlayer(bestTarget, player);
-            bestTarget.sendSystemMessage(Component.literal("You have been revived by " + player.getName().getString()).withStyle(ChatFormatting.GREEN));
-            bestTarget.sendSystemMessage(Component.literal("You revived " + bestTarget.getName().getString()).withStyle(ChatFormatting.GREEN));
+            ReviveAPI.revivePlayer(target, player);
+            target.sendSystemMessage(Component.literal("You have been revived by " + player.getName().getString()).withStyle(ChatFormatting.GREEN));
+            target.sendSystemMessage(Component.literal("You revived " + target.getName().getString()).withStyle(ChatFormatting.GREEN));
             ReviveManager.removeItem(event.getItemStack(),player);
         }
     }
